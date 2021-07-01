da07470265c5f627cc232429bdf3047438434bb0
Dan Wetherald <dan.wetherald@gmail.com>
2021-06-30 19:35:11 -0500

    no async/await

--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@ const sensor = require("ds18x20")
 
 let main = () => {
   sensor.isDriverLoaded(function (err, isLoaded) {
-    console.log(isLoaded);
+    console.log("Loaded: ", isLoaded)
+
+    sensor.list(function (err, listOfDeviceIds) {
+      console.log(listOfDeviceIds)
+    })
   })
 }
 
