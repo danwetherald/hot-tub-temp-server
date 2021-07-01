f2af14a81686fb1faf1dcc2e2c51e51dceffc0cb
Dan Wetherald <dan.wetherald@gmail.com>
2021-06-30 19:45:18 -0500

    express

--- src/server.js
+++ src/server.js
@@ -1,21 +1,43 @@
 const sensor = require("ds18x20")
+const express = require("express")
 
-let main = () => {
+const app = express()
+
+const port = 8000
+
+let getTemp = () => {
   sensor.isDriverLoaded(function (err, isLoaded) {
-    console.log("Loaded: ", isLoaded)
+    if (err) return err
 
     sensor.list(function (err, listOfDeviceIds) {
-      console.log(listOfDeviceIds)
+      if (err) return err
 
       if (listOfDeviceIds.length > 0) {
         sensor.get(listOfDeviceIds[0], function (err, temp) {
           const fahrenheit = (temp * 1.8) + 32
-
           console.log(`${fahrenheit}°F`)
+
+          return fahrenheit
         })
+      } else {
+        return new Error("No Devices")
       }
     })
   })
 }
 
-main()
+app.get(
+  "/data",
+  express.json({ type: "application/json" }),
+  async (req: any, res: any) => {
+    const tempature = getTemp()
+
+    res.send({
+      tempature
+    })
+  }
+)
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
