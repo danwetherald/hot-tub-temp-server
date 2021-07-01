37ba6e9bd77ce591ade6bdc954e1a757e834086e
Dan Wetherald <dan.wetherald@gmail.com>
2021-06-30 19:34:11 -0500

    no async/await

--- src/server.js
+++ src/server.js
@@ -1,16 +1,9 @@
 const sensor = require("ds18x20")
 
 let main = () => {
-  try {
-    sensor.loadDriver()
-
-    const sensors = sensor.list()
-
-    console.log(sensors)
-
-  } catch (e) {
-    console.error(e)
-  }
+  sensor.isDriverLoaded(function (err, isLoaded) {
+    console.log(isLoaded);
+  })
 }
 
 main()
